9aef99bc5dc0e3b1499f575124aa23c70d7391d9
Javier Vargas <thebruh@Javiers-MacBook-Air.local>
2022-07-22 15:23:25 -0400

    read from firestore 3 ways

--- index.js
+++ index.js
@@ -12,11 +12,56 @@ initializeApp({
 
 const db = getFirestore()
 
-const car = {
-  make: "toyota",
-  model: "AE86",
-  year: 2018,
-  color: "white",
-}
+// const car2 = {
+//   make: "Ferrari",
+//   model: "GTO",
+//   year: 2008,
+//   color: "red",
+// }
+// db.collection("cars").add(car2)
+//     .then(doc => {
+//         console.log('Doc added: ',doc.id)
+//     })
+//     .catch(err => console.error(err))
 
-db.collection("cars").add(car)
+// db.collection('cars').doc('lambo')
+// .set({make: 'Lamborghini', model: 'Diablo', year: 2020, color: 'yellow'})
+
+// db.collection('cars'). doc('lambo')
+// .update({ model: 'Diablo', color: 'hot pink'})
+
+//get a single document:
+db.collection("cars")
+  .doc("lambo")
+  .get()
+  .then((doc) => {
+    console.log(doc.id)
+    console.log(doc.data())
+  })
+  .catch(console.error)
+
+//get a whole collection:
+db.collection("cars")
+  .get()
+  .then((collection) => {
+    collection.docs.forEach((doc) => console.log(doc.id, doc.data()))
+  })
+  .catch(console.error)
+
+//Query docs from collection
+db.collection("cars")
+  .where("year", ">=", 2015)
+  .get()
+  .then((collection) => {
+    const cars = collection.docs.map((doc) => {
+      let car = doc.data()
+      car.id = doc.id
+      return car
+    })
+    //THE PRO WAY
+    // const cars2 = collection.docs.map(doc => {
+    //     return{...doc.data(), id: doc.id}
+    // })
+    console.log(cars)
+  })
+  .catch(console.error)
